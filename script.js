989cf498becf55094031a47edca1ed9414ce2092
C4L3B3666 <calebec055@gmail.com>
2025-07-11 21:37:20 +0100

    Estilização do menu lateral

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-var menu = document.querySelector("header");
 var menuHamburguer = document.querySelector("#nav_menu_hamburguer")
+
+menuHamburguer.addEventListener("click", ()=> {
+    var menu = document.querySelector("header");
+    menu.classList.toggle("mostrar_menu_lateral")
+
+})
